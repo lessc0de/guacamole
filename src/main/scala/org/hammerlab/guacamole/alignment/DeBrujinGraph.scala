a90439536ef6841c5ac25b353e03d441e78a0d4b
arahuja <aahuja11@gmail.com>
2015-01-28 09:12:04 -0500

    add scoring of paths

--- src/main/scala/org/hammerlab/guacamole/alignment/DeBrujinGraph.scala
+++ src/main/scala/org/hammerlab/guacamole/alignment/DeBrujinGraph.scala
@@ -7,17 +7,24 @@ class DeBrujinGraph(val kmerCounts: mutable.Map[Kmer, Int], kmerSize: Int) {
 
   type Path = List[Kmer]
 
+  def pruneKmers(minSupport: Int) = {
+    kmerCounts
+      .filter(_._2 < minSupport)
+      .foreach( {case (kmer, count) => kmerCounts.remove(kmer) })
+  }
+
   def allPaths(seed: Kmer,
-                       minPathLength: Int = 100,
-                       maxPathLength: Int = 1000,
-                       maxPaths: Int = 10): List[Path] = {
-    var paths: List[Path] = List.empty[Path]
+               minPathLength: Int = 100,
+               maxPathLength: Int = 1000,
+               maxPaths: Int = 10): List[(Path, Int)] = {
+    var paths: List[(Path, Int)] = List.empty[(Path, Int)]
     var frontier: mutable.Stack[Kmer] = mutable.Stack(seed)
     var currentPath: Path = List.empty
-
+    var pathScore: Int = 0
     // explore branches until we accumulate the maximum number of appropriate length paths
     while (frontier.nonEmpty && paths.size < maxPaths) {
       val next = frontier.pop()
+      pathScore += kmerCounts(next)
       currentPath = next :: currentPath
       val nextChildren = children(next)
       if (nextChildren.nonEmpty && currentPath.size < maxPathLength) {
@@ -25,11 +32,11 @@ class DeBrujinGraph(val kmerCounts: mutable.Map[Kmer, Int], kmerSize: Int) {
       } else {
         // avoid short branches and repetitive branches
         if (currentPath.size + 1 >= minPathLength && currentPath.size < maxPathLength)
-          paths = currentPath :: paths
+          paths = (currentPath, pathScore) :: paths
         currentPath = List.empty
+        pathScore = 0
       }
     }
-
     paths
   }
 
